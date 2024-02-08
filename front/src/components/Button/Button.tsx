e8ddbd456f95c7e023e789a4885277ec1647fdcc
eumyang99 <eumyang99@gmail.com>
2024-02-08 13:40:58 +0900

    #26 Fix: 수정 - ButtonProps에서 setIsActive를 onClick 함수로 대체

--- front/src/components/Button/Button.tsx
+++ front/src/components/Button/Button.tsx
@@ -2,38 +2,18 @@
 
 import styles from "./Button.module.scss";
 
-// string 일 때,
-// content : 버튼 텍스트
-// isActive : store 상태값
-// setIsActive : store 상태 변화 함수
-
-// icon 일 때,
-// content : icon 컴포넌트
-// setIsActive : store 상태 변화 함수
 type ButtonProps = {
   content: string | React.ReactNode;
   isActive?: boolean;
-  setIsActive?: () => void;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 };
-export default function Button({
-  content,
-  isActive,
-  setIsActive,
-}: ButtonProps) {
-  const handleClick = () => {
-    // 상태 변화 함수가 있는 경우
-    if (setIsActive) {
-      setIsActive();
-    }
-    // 상태 변화 함수가 없는 경우, 다른 로직
-  };
-
+export default function Button({ content, isActive, onClick }: ButtonProps) {
   return (
     <button
       className={`${styles.button} ${
         typeof content === "string" && isActive ? styles.active : ""
       }`}
-      onClick={handleClick}
+      onClick={onClick}
     >
       {content}
     </button>
