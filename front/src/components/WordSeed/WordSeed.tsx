5a5468fcbb9d4ae09791fd34a933bc51e45c4652
황상윤 <s_infinite@naver.com>
2024-02-09 05:44:11 +0900

    #27 Feat: 구현 - WordSeed-Component 구현

--- front/src/components/WordSeed/WordSeed.tsx
+++ front/src/components/WordSeed/WordSeed.tsx
@@ -1,12 +1,23 @@
+"use client";
+
 import styles from "./WordSeed.module.scss";
+import { useRouter } from "next/navigation";
 
 type WordSeedProps = {
   date: string;
   wordSeed: string;
 };
 export default function WordSeed({ date, wordSeed }: WordSeedProps) {
+  const router = useRouter();
+  const wordSeedLink = `/wordseed/${wordSeed}`;
+
   return (
-    <div className={styles.container}>
+    <div
+      onClick={() => {
+        router.push(wordSeedLink);
+      }}
+      className={styles.container}
+    >
       <p className={styles.date}>{date}</p>
       <p className={styles.wordSeed}>{wordSeed}</p>
     </div>
