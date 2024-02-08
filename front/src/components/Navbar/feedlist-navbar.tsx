ca7fff3ce315bd28270456a488dbb688a456305a
eumyang99 <eumyang99@gmail.com>
2024-02-09 04:42:13 +0900

    #29 Feat: 구현 - feedlist 페이지에 filter 컴포넌트 장착

--- front/src/components/Navbar/feedlist-navbar.tsx
+++ front/src/components/Navbar/feedlist-navbar.tsx
@@ -3,14 +3,24 @@
 import IncludeMenu from "./include-menu/include-menu";
 import FilterButton from "../Button/filter-button";
 import CreateFeedButton from "../Button/create-feed-button";
+import Filter from "../filter/filter";
 import styles from "./navbar.module.scss";
+import { useState } from "react";
 
 export default function FeedListNavbar() {
+  const [isHidden, setIsHidden] = useState(true);
   return (
     <IncludeMenu>
       <div className={styles["right-icon-container"]}>
-        <FilterButton />
+        <FilterButton setIsHidden={setIsHidden} />
         <CreateFeedButton />
+      </div>
+      <div
+        className={`${styles["filter-container"]} ${
+          isHidden ? styles["hidden"] : ""
+        }`}
+      >
+        <Filter />
       </div>
     </IncludeMenu>
   );
