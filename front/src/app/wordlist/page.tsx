d954e4fa808996f07b764a17c33e157dba0cb3c6
황상윤 <s_infinite@naver.com>
2024-02-09 04:26:23 +0900

    #28 Feat: 구현 - WordSeedList Component 구현

--- front/src/app/wordlist/page.tsx
+++ front/src/app/wordlist/page.tsx
@@ -1,3 +1,64 @@
+import { WordSeedList } from "@/components";
+
+const datas: {
+  date: string;
+  wordSeed: string;
+}[] = [
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+  {
+    date: "2024년 2월 9일",
+    wordSeed: "한 걸음",
+  },
+];
+
 export default function Wordlist() {
-  return <h1>Wordlist</h1>;
+  return (
+    <>
+      <h1>Wordlist</h1>
+      <WordSeedList datas={datas} />
+    </>
+  );
 }
--- front/src/components/index.ts
+++ front/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Icon } from "./Icon/Icon";
 export { default as WordSeed } from "./WordSeed/WordSeed";
+export { default as WordSeedList } from "./WordSeedList/WordSeedList";
