ce637616765cd26c48f96c8f3aabc5f6fdc21037
eumyang99 <eumyang99@gmail.com>
2024-02-11 21:27:19 +0900

    #29 Feat: 구현 - create 페이지 작품 종류 선택 컴포넌트

--- front/src/components/Navbar/create-navbar.tsx
+++ front/src/components/Navbar/create-navbar.tsx
@@ -1,8 +1,13 @@
 "use client";
-
+import CreateCategory from "./create-category/create-category";
 import Button from "../Button/Button";
 import styles from "./navbar.module.scss";
+import { useState } from "react";
+
 export default function CreateNavbar() {
+  const [selectedCategory, setSelectedCategory] = useState("글");
+  // selectedCategory 형식으로 게시글 업로드 요청
+
   const handleCloseButtonClick = (e: React.MouseEvent) => {
     // 뒤로가기
   };
@@ -10,11 +15,16 @@ export default function CreateNavbar() {
     // 작성된 내용 post 요청
   };
   return (
-    <div className={styles.navbar}>
-      <Button content={"닫기"} onClick={handleCloseButtonClick} />
-      <header className={styles.wordseed}>해당 말씨</header>
-      <Button content={"완료"} onClick={handleSubmitButtonClick} />
+    <div>
+      <div className={styles.navbar}>
+        <Button content={"닫기"} onClick={handleCloseButtonClick} />
+        <header className={styles.wordseed}>해당 말씨</header>
+        <Button content={"완료"} onClick={handleSubmitButtonClick} />
+      </div>
+      <CreateCategory
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
     </div>
-    // 작품 종류 선택 옵션
   );
 }
