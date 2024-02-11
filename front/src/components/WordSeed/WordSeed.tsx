d28efedbe64c037ab94e06cd19be3b2961fbf4a4
황상윤 <s_infinite@naver.com>
2024-02-12 02:57:41 +0900

    #46 수정 - WordSeed Component router => Link 수정

--- front/src/components/WordSeed/WordSeed.tsx
+++ front/src/components/WordSeed/WordSeed.tsx
@@ -1,25 +1,17 @@
 "use client";
 
+import Link from "next/link";
 import styles from "./WordSeed.module.scss";
-import { useRouter } from "next/navigation";
 
 type WordSeedProps = {
   date: string;
   wordSeed: string;
 };
 export default function WordSeed({ date, wordSeed }: WordSeedProps) {
-  const router = useRouter();
-  const wordSeedLink = `/wordseed/${wordSeed}`;
-
   return (
-    <div
-      onClick={() => {
-        router.push(wordSeedLink);
-      }}
-      className={styles.container}
-    >
+    <Link href={`/wordseed/${wordSeed}`} className={styles.container}>
       <p className={styles.date}>{date}</p>
       <p className={styles.wordSeed}>{wordSeed}</p>
-    </div>
+    </Link>
   );
 }
