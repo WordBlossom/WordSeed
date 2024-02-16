a97ed585a7075eaf44eccdbe3509f3cfc0b6b719
socm9145 <socm9145@naver.com>
2024-02-16 20:47:43 +0900

    Init: 설정 - Spring, H2 환경 구축 [#52]

---
+++ back/wordseed/src/test/java/com/spring/wordseed/WordseedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.wordseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordseedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
