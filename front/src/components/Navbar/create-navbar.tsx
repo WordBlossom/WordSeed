1f6900a5ce39f5c0da2c10325f2b6b0bcf772544
황상윤 <s_infinite@naver.com>
2024-02-22 19:07:43 +0900

    #85 Refactor: 수정 - create navbar 말씨 유니코드 한글로 변환

--- front/src/components/Navbar/create-navbar.tsx
+++ front/src/components/Navbar/create-navbar.tsx
@@ -3,20 +3,23 @@ import CreateCategory from "./create-category/create-category";
 import Button from "../Button/Button";
 import styles from "./navbar.module.scss";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 export default function CreateNavbar() {
   const router = useRouter();
   const [selectedCategory, setSelectedCategory] = useState("글");
+  const params = useParams();
+  const wordseed = decodeURIComponent(params.wordseed as string);
   // selectedCategory 형식으로 게시글 업로드 요청
 
   const handleSubmitButtonClick = (e: React.MouseEvent) => {
     // 작성된 내용 post 요청
   };
+
   return (
     <div>
       <div className={styles.navbar}>
         <Button content={"닫기"} onClick={() => router.back()} />
-        <header className={styles.wordseed}>해당 말씨</header>
+        <header className={styles.wordseed}>{wordseed}</header>
         <Button content={"완료"} onClick={handleSubmitButtonClick} />
       </div>
       <CreateCategory
