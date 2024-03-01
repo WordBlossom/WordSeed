4f8559e6940009bc8aa03acf0334c409f316a775
eumyang99 <eumyang99@gmail.com>
2024-03-01 14:01:32 +0900

    #146 feat: 구현 - useWordseedList 사용해서 WordseedList 컴포넌트에 무한스크롤 장착

--- front/src/components/WordSeedList/WordSeedList.tsx
+++ front/src/components/WordSeedList/WordSeedList.tsx
@@ -1,21 +1,49 @@
-import { WordSeed } from "..";
+"use client";
+import { useInView } from "react-intersection-observer";
+import Wordseed from "@/components/WordSeed/WordSeed";
+import NoData from "./component/no-data";
+import useSearchPageStateStore from "@/stores/search-page";
+import { useWordseedList } from "@/api/wordseed";
 import styles from "./WordSeedList.module.scss";
 
-type WordSeedProps = {
-  date: string;
-  wordSeed: string;
-};
+// route 판별해서 말씨 목록 페이지이면 모든 데이터 조회하는 요청
+// search 페이지이면 아래와 같이 요청
 
-type WordSeedListProps = {
-  datas: WordSeedProps[];
-};
+export default function WordSeedList() {
+  const { searchKeyword } = useSearchPageStateStore();
+  const { status, data, fetchNextPage } = useWordseedList({
+    params: {
+      query: searchKeyword,
+    },
+  });
+  const [ref] = useInView({
+    onChange: (inView) => {
+      if (inView) fetchNextPage();
+    },
+  });
 
-export default function WordSeedList({ datas }: WordSeedListProps) {
   return (
     <div className={styles.container}>
-      {datas.map((data) => (
-        <WordSeed key={data.date} date={data.date} wordSeed={data.wordSeed} />
-      ))}
+      {/* {status === "pending" && <p>Loading...</p>} */}
+      {status === "success" && data.pages.length === 0 && (
+        <NoData searchKeyword={searchKeyword} />
+      )}
+      {status === "success" && (
+        <>
+          {data.pages.map((wordseed, idx) => (
+            <div
+              key={idx}
+              ref={idx === data.pages.length - 5 ? ref : undefined}
+            >
+              <Wordseed
+                // 나중에는 key값을 날짜로 수정해야함
+                date={wordseed.createdAt}
+                wordseed={wordseed.word}
+              />
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
