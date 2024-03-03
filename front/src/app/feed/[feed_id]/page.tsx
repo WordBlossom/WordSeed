16952eb9857d254d59889fbe049be5c4d523194d
eumyang99 <eumyang99@gmail.com>
2024-03-03 15:59:25 +0900

    #155 feat: 구현 - feed page에 작품 컴포넌트 장착

--- front/src/app/feed/[feed_id]/page.tsx
+++ front/src/app/feed/[feed_id]/page.tsx
@@ -1,9 +1,13 @@
+import { FeedInterface } from "@/components";
+import FeedContent from "@/components/feed/feed";
+import style from "./feed.module.scss";
+
 export default function Feed({ params }: { params: { feed_id: string } }) {
   const feedId = params.feed_id;
   return (
-    <>
-      <h1>Feed</h1>
-      <h2>feedId : {feedId}</h2>
-    </>
+    <div className={style["feed-container"]}>
+      <FeedContent />
+      <FeedInterface />
+    </div>
   );
 }
