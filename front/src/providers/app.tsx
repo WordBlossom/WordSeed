3a6de326003d765262c2d0de044699750296e681
eumyang99 <eumyang99@gmail.com>
2024-03-04 10:52:20 +0900

    #160 refactor: 수정 - SSR queryClient 적용

--- front/src/providers/app.tsx
+++ front/src/providers/app.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import * as React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { queryClient } from "@/lib/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { getQueryClient } from "@/lib/react-query";
 import RNListener from "./react-native-listener";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
@@ -11,18 +11,7 @@ type AppProviderProps = {
 };
 
 export const AppProvider = ({ children }: AppProviderProps) => {
-  const [queryClient] = React.useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            // With SSR, we usually want to set some default staleTime
-            // above 0 to avoid refetching immediately on the client
-            staleTime: 60 * 1000,
-          },
-        },
-      })
-  );
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
