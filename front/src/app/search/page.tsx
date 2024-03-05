338a76142d216e45d868d21843a3fc658c3297b2
eumyang99 <eumyang99@gmail.com>
2024-03-05 17:59:18 +0900

    #154 refactor: 수정 - search 페이지 컴포넌트 index.ts 추가

--- front/src/app/search/page.tsx
+++ front/src/app/search/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-
-import SearchButtons from "./_component/search-buttons";
-import SearchList from "./_component/search-list";
+import { SearchButtons, SearchList } from "./_component/";
 import style from "./_component/search.module.scss";
 
 export default function Search() {
