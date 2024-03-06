6b6580b59ed161292cd0bcbac09c4e472383a0e9
socm9145 <socm9145@naver.com>
2024-03-06 11:37:46 +0900

    Fix: 수정 - allowedMethods 추가

--- back/wordseed/src/main/java/com/spring/wordseed/config/WebConfig.java
+++ back/wordseed/src/main/java/com/spring/wordseed/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.spring.wordseed.interceptor.AuthInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,7 +24,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(CLIENT_URL)
-                .exposedHeaders("GET", "POST", "PUT", "DELETE")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                )
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
