40334750040c8ae10a099a52004fea9f532c784f
eumyang99 <eumyang99@gmail.com>
2024-03-07 15:20:46 +0900

    #187 feat: 구현 - 무한 스크롤 페이지 SSR hook 구현 - prefetchInfiniteQuery 사용

--- front/src/api/feed/get-feed-list.ts
+++ front/src/api/feed/get-feed-list.ts
@@ -1,19 +1,30 @@
 import { axios } from "@/lib/axios";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { FeedListDTO, FeedList } from "./types";
+import {
+  QueryClient,
+  useInfiniteQuery,
+  dehydrate,
+} from "@tanstack/react-query";
 import { InfiniteQueryConfig } from "@/lib/react-query";
+import { FeedListDTO, FeedList } from "./types";
 
 const REQUEST_SIZE = 5;
+export const DEFAULT_POST_TYPE = "TEXT,PAINT,VIDEO,MUSIC";
+export const DEFAULT_SORT = "DATE_DSC";
 
 export async function getFeedList(params: FeedListDTO): Promise<FeedList> {
   return axios.get(`/post/list/word`, { params });
 }
 
 type QueryFnType = typeof getFeedList;
-type UseFeedListOptions = {
+
+interface UseFeedListOptions {
   params: FeedListDTO;
   config?: InfiniteQueryConfig<QueryFnType>;
-};
+}
+
+interface PrefetchUseFeedListOptions extends UseFeedListOptions {
+  queryClient: QueryClient;
+}
 
 export function useFeedList({ params, config }: UseFeedListOptions) {
   return useInfiniteQuery({
@@ -21,7 +32,7 @@ export function useFeedList({ params, config }: UseFeedListOptions) {
     queryKey: ["FeedList", params],
     queryFn: ({ pageParam }) =>
       getFeedList({ ...params, size: REQUEST_SIZE, page: pageParam }),
-    initialPageParam: 1,
+    initialPageParam: 2,
     getNextPageParam: (lastPage, allPages) => {
       const morePagesExist = lastPage?.posts?.length === REQUEST_SIZE;
       if (!morePagesExist) return undefined;
@@ -32,4 +43,25 @@ export function useFeedList({ params, config }: UseFeedListOptions) {
       pages: data.pages.flatMap((page) => page.posts),
     }),
   });
+}
+
+export async function usePrefetchFeedList({
+  queryClient,
+  params,
+  config,
+}: PrefetchUseFeedListOptions) {
+  await queryClient.prefetchInfiniteQuery({
+    ...config,
+    queryKey: ["FeedList", params],
+    queryFn: ({ pageParam }) =>
+      getFeedList({ ...params, size: 5, page: pageParam }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      const morePagesExist = lastPage?.posts?.length === 5;
+      if (!morePagesExist) return undefined;
+      return allPages.length + 1;
+    },
+    pages: 1,
+  });
+  return dehydrate(queryClient);
 }
