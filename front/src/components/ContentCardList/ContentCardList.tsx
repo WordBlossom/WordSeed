66a3589eed757fa15794b823a87f1d87930d70b6
황상윤 <s_infinite@naver.com>
2024-03-10 02:11:50 +0900

    #152 feat: 구현 - profile feed 무한 스크롤 구현

--- front/src/components/ContentCardList/ContentCardList.tsx
+++ front/src/components/ContentCardList/ContentCardList.tsx
@@ -1,58 +1,10 @@
 import useSearchFilterStateStore from "@/stores/search-filter";
+import { useInView } from "react-intersection-observer";
 import { ContentCard } from "..";
 import styles from "./ContentCardList.module.scss";
 import { FeedListDTO, FeedTypeEnum, UserFeedListDTO } from "@/api/feed/types";
 import profileToggleStore from "@/stores/profile-toggle";
 import { useFeedList } from "@/api/feed";
-
-interface ContentCardProps {
-  postId: number;
-  type: "text" | "img" | "video" | "sound";
-  title: string;
-  content: string;
-  textAlign: "start" | "center" | "end";
-  userName: string;
-  url: string;
-}
-
-const datas: ContentCardProps[] = [
-  {
-    postId: 1,
-    type: "text",
-    title: "자기 소개",
-    content: "어설프어설프게 시작한 소개는\n 그렇게 나를 인식시킨다.",
-    textAlign: "center",
-    userName: "초아누리",
-    url: "https://velog.velcdn.com/images/shrewslampe/post/b455111f-8921-4a48-908f-f5fea743d286/image.png",
-  },
-  {
-    postId: 2,
-    type: "img",
-    title: "자기 소개",
-    content: "어설프어설프게 시작한 소개는\n 그렇게 나를 인식시킨다.",
-    textAlign: "center",
-    userName: "초아누리",
-    url: "https://velog.velcdn.com/images/shrewslampe/post/b455111f-8921-4a48-908f-f5fea743d286/image.png",
-  },
-  {
-    postId: 3,
-    type: "video",
-    title: "자기 소개",
-    content: "어설프어설프게 시작한 소개는\n 그렇게 나를 인식시킨다.",
-    textAlign: "center",
-    userName: "초아누리",
-    url: "https://i2.ruliweb.net/ori/24/02/17/18db50369258db08.mp4",
-  },
-  {
-    postId: 4,
-    type: "video",
-    title: "자기 소개",
-    content: "어설프어설프게 시작한 소개는\n 그렇게 나를 인식시킨다.",
-    textAlign: "center",
-    userName: "초아누리",
-    url: "https://i2.ruliweb.net/ori/24/02/15/18dab844b3f484f84.mp4",
-  },
-];
 
 export default function ContentCardList({ userId }: { userId: number }) {
   const myId = 4;
@@ -90,13 +42,24 @@ export default function ContentCardList({ userId }: { userId: number }) {
 
   const { data, status, fetchNextPage } = useFeedList(props);
 
+  const [ref] = useInView({
+    onChange: (inView) => {
+      if (inView) fetchNextPage();
+    },
+  });
+
   return (
     <>
       {status === "success" && !data.pages.length && <p>결과 없음</p>}
       {status === "success" && data.pages.length && (
         <div className={styles.container}>
-          {data.pages.map((feedData) => (
-            <ContentCard key={feedData.postId} {...feedData} />
+          {data.pages.map((feedData, idx) => (
+            <div
+              key={feedData.postId}
+              ref={idx === data.pages.length - 5 ? ref : undefined}
+            >
+              <ContentCard key={feedData.postId} {...feedData} />
+            </div>
           ))}
         </div>
       )}
