213f22d9442646e6db998765e09523bd28537c93
eumyang99 <eumyang99@gmail.com>
2024-03-11 16:45:37 +0900

    #190 refactor: 수정 - 개별 작품 컴포넌트 내부에서 이벤트 리스너를 장착할 요소를 useRef로 특정

--- front/src/components/feed-interface/feed-interface.tsx
+++ front/src/components/feed-interface/feed-interface.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+"use client";
+
+import React, { MutableRefObject } from "react";
+import { useSwiper, useSwiperSlide } from "swiper/react";
 import FeedInterfaceTop from "./feed-interface-top";
 import FeedInterfaceBottom from "./feed-interface-bottom";
 import { FeedDetail } from "@/api/feed/types";
 import styles from "./feed-interface.module.scss";
 
 type FeedInterfaceProps = {
+  activeInterfaceRef: MutableRefObject<HTMLDivElement | null>;
   feedData: FeedDetail;
 };
-function FeedInterface({ feedData }: FeedInterfaceProps) {
+function FeedInterface({ activeInterfaceRef, feedData }: FeedInterfaceProps) {
+  const slide = useSwiperSlide();
   return (
-    <div className={styles["feed-interface-container"]}>
+    <div
+      ref={slide.isActive ? activeInterfaceRef : null}
+      className={styles["feed-interface-container"]}
+    >
       <FeedInterfaceTop createdAt={feedData.createdAt} />
       <FeedInterfaceBottom feedData={feedData} />
     </div>
