688e96ac18d87d3a1fbda487b2679660211724d3
eumyang99 <eumyang99@gmail.com>
2024-03-15 05:41:15 +0900

    #208 feat: 구현 - 댓글 버튼 클릭 시 해당 작품 ID를 store에 저장

--- front/src/components/comment/comment.tsx
+++ front/src/components/comment/comment.tsx
@@ -1,16 +1,19 @@
-import styles from "./comment.module.scss";
+import useCommentToggleStateStore from "@/stores/comment-toggle";
 import CommentClose from "./comment-close";
 import CommentContainer from "./comment-container";
 import CommentInput from "./comment-input";
+import styles from "./comment.module.scss";
+
 export default function Comment() {
+  const { commentPostId } = useCommentToggleStateStore();
   // feedlist page에서 작품 id를 prop 받고
   // container에 id 넘겨서 infinity query
   // input에 id 넘겨서 댓글 등록
   return (
     <div className={styles["comment"]}>
       <CommentClose />
-      <CommentContainer />
-      <CommentInput />
+      <CommentContainer commentPostId={commentPostId} />
+      <CommentInput commentPostId={commentPostId} />
     </div>
   );
 }
