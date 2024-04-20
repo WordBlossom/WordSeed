2d15870f04e25678f0776487c6045eaea15bd213
eumyang99 <eumyang99@gmail.com>
2024-04-21 05:43:19 +0900

    #223 refacotr: 수정 - feedlist 페이지 재활용 말씨 기반 작품 목록 페이지를 재활용 dynamic routes를 활용해서 폴더명을 [word_id] -> [[...word_id]] 로 변경 word_id가 params로 감지되면 말씨 기반 작품 목록을 요청 및 렌더링 word_id가 감지되지 않으면 관심 작가 작품 목록을 요청 및 렌더링

---
+++ front/src/app/feedlist/[[...word_id]]/layout.tsx
@@ -0,0 +1,40 @@
+import { HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import {
+  usePrefetchFeedList,
+  DEFAULT_POST_TYPE,
+  DEFAULT_SORT,
+} from "@/api/feed/";
+import { MainFeedListDTO } from "@/api/feed/types";
+
+type FeedLayoutProps = {
+  children: React.ReactNode;
+  params: { word_id?: number[] };
+};
+
+export default async function FeedLayout({
+  children,
+  params,
+}: FeedLayoutProps) {
+  const wordId = params.word_id && params.word_id[0];
+
+  const queryClient = new QueryClient();
+  const defaultParams: MainFeedListDTO = {
+    postType: DEFAULT_POST_TYPE,
+    sort: DEFAULT_SORT,
+  };
+  if (wordId) {
+    defaultParams["wordId"] = wordId;
+  }
+
+  const dehydratedQueryClient = await usePrefetchFeedList({
+    queryClient: queryClient,
+    params: defaultParams,
+    type: wordId ? "word" : "follow",
+  });
+
+  return (
+    <HydrationBoundary state={dehydratedQueryClient}>
+      {children}
+    </HydrationBoundary>
+  );
+}
