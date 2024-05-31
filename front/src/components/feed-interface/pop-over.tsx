649b00cf02d24d0271ccac1f1a31584ac4382afc
eumyang99 <eumyang99@gmail.com>
2024-05-31 14:19:50 +0900

    #231 feat: 삭제 버튼에 useDeleteFeed 장착

--- front/src/components/feed-interface/pop-over.tsx
+++ front/src/components/feed-interface/pop-over.tsx
@@ -1,10 +1,23 @@
+import { useDeleteFeed } from "@/api/feed/hooks/delete-feed";
+import Button from "@/components/Button/Button";
+import { FeedDetail, FeedList, InfiniteQueriesUpdater } from "@/api/feed/types";
 import styles from "./feed-interface.module.scss";
 
 type PopoverProps = {
   showPopover: boolean;
+  postId: FeedDetail["postId"];
+  wordId: FeedDetail["wordId"];
+  postType: FeedDetail["postType"];
 };
 
-export default function Popover({ showPopover }: PopoverProps) {
+export default function Popover({
+  showPopover,
+  wordId,
+  postId,
+  postType,
+}: PopoverProps) {
+  const deleteFeed = useDeleteFeed({ postId, wordId, postType });
+
   return (
     <div
       className={`${
@@ -12,7 +25,7 @@ export default function Popover({ showPopover }: PopoverProps) {
       }`}
     >
       <li>수정</li>
-      <li>삭제</li>
+      <Button content={"삭제"} onClick={() => deleteFeed.mutate()} />
     </div>
   );
 }
