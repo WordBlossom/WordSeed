4d51d5340b3d3c8a64037109e8a4abbf6b6a8d24
eumyang99 <eumyang99@gmail.com>
2024-06-14 22:51:22 +0900

    #251 refactor: wordseed 페이지 SSR 적용 - wordseed가 늦게 렌더링 되는 문제가 있어서 SSR로 해결

--- front/src/app/wordseed/[date]/page.tsx
+++ front/src/app/wordseed/[date]/page.tsx
@@ -1,9 +1,24 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getWordseed } from "@/api/wordseed";
 import WordseedMain from "@/components/wordseed-main/wordseed-main";
 
 type WordseedParams = {
   params: { date: string };
 };
 
-export default function Wordseed({ params }: WordseedParams) {
-  return <WordseedMain date={params.date} />;
+export default async function Wordseed({ params }: WordseedParams) {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["wordseed", params.date],
+    queryFn: () => getWordseed(params.date),
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <WordseedMain date={params.date} />;
+    </HydrationBoundary>
+  );
 }
